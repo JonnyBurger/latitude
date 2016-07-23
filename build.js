e1f9a63c3fc3c10dd2e8a759f10fc6d266a3a401
Jonny Burger <jonathanburger11@gmail.com>
2016-07-23 17:19:42 +0200

    Fix

--- build.js
+++ build.js
@@ -3,7 +3,8 @@ const co = require('co');
 
 co(function *() {
 	yield execa('xcodebuild', null, {cwd: 'whereami'});
-	yield execa('mv', ['whereami/build/Release/whereami', 'fetcher']);
+	yield execa('mkdir', ['-p', 'bin']);
+	yield execa('mv', ['whereami/build/Release/whereami', 'bin/whereami']);
 	yield execa('rm', ['-rf', 'whereami/build']);
 })
 .catch(err => {
--- index.js
+++ index.js
@@ -6,11 +6,10 @@ const co = require('co');
 module.exports = function () {
 	return new Promise(function (resolve, reject) {
 		co(function *() {
-			let data = yield execa('./fetcher');
+			let data = yield execa('whereami');
 			if (data.stderr.length) {
 				throw new Error(data.stderr);
 			}
-			console.log(data.stdout);
 			const result = JSON.parse(data.stdout);
 			if (result.success) {
 				resolve(result.location);
